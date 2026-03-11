54e85e6d9f0edb1ca8dcad7a97eb41f91f8f2248
Pepino8 <cristobalcoronador@gmail.com>
2026-03-10 21:45:34 -0700

    ParametizedCalculatorTest

---
+++ src/test/java/mx/edu/cetys/software_quality_lab/poc/ParametizedCalculatorTest.java
@@ -0,0 +1,86 @@
+package mx.edu.cetys.software_quality_lab.poc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParametizedCalculatorTest {
+
+    // @Test
+    @DisplayName("Test Sum of 2 parameters with CSV Source")
+    @ParameterizedTest
+    @CsvSource({
+            "10,10,20",
+            "0,0,0,5", //extra params
+            "-5,5,0",
+            "'1','2','3'"
+    })
+    void testSumWithCsvSource(int a, int b, int expected) {
+        //arrange
+        //act
+        var sum = a + b;
+        assertEquals(expected,sum);
+    }
+
+    @DisplayName("Validate String not empty")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "hello",
+            "world",
+            "ramirami"
+    })
+    void testValidateStringNotEmpty(String values) {
+        var isEmpty = values.isEmpty();
+        assertFalse(isEmpty);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Validate double of an integer with MethodSource")
+    @MethodSource("provideNumbers")
+    void testDouble(int a, int expected){
+        //Act
+        //var doubleValue = a + a;
+        var doubleValue = a * 2;
+        assertEquals(expected,doubleValue);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Validate Pet is older than 10 ")
+    @MethodSource("providePets")
+    void testDouble(Pet pet, boolean expected){
+        // Act
+        var isOlderThanTen = pet.age() > 10 ? true : false;
+        assertEquals(expected, isOlderThanTen);
+    }
+
+    public static Stream<Object[]> provideNumbers() {
+        return Stream.of(
+                new Object[]{2,4},
+                new Object[]{5,10}
+                //new Object[]{"Hola", "HolaHola"}
+                );
+    }
+
+    public static Stream<Object[]> providePets() {
+        return Stream.of(
+                new Object[]{new Pet("Jose", "Black", 14, "perro"), true},
+                new Object[]{new Pet(null, null, 10, null), false},
+                new Object[]{new Pet(null, null, 9, null), false},
+                new Object[]{new Pet(null, null, 11, null), true}
+
+
+        //new Object[]{"Hola", "HolaHola"}
+        );
+    }
+
+    //POJO = Plain Old Java Object
+    //Records = POJO inmutable sin BoilerPlate
+    private record Pet(String name, String color, int age, String race){}
+}
